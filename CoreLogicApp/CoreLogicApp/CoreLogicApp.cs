264b854c756c3ffed9a4cf5ee1d27399257f20ea
Nikita Pustovoit <52323913+SpoiledBogdan@users.noreply.github.com>
2023-03-27 11:14:39 +0300

    Add files via upload

---
+++ CoreLogicApp/CoreLogicApp/CoreLogicApp.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreLogicApp
+{
+    internal class CoreLogicApp
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+
+            Console.WriteLine("Введите количество рельсовых цепей: ");
+            int count = Convert.ToInt32(Console.ReadLine());
+
+            RK[] RKs = new RK[count];
+            //RL[] RLs = new RL[count];
+            //PK[] PKs = new PK[count];
+
+            while (i < count)
+            {
+                RKs[i] = new RK();
+                Console.WriteLine($"Введите для {i+1}-го РК параметр: ");
+                RKs[i].ParameterRK = int.Parse(Console.ReadLine());
+
+                i++;
+            }
+
+            Console.WriteLine($"Значение параметра РК 3 равно {RKs[2].ParameterRK}");
+        }
+    }
+}
---
+++ CoreLogicApp/CoreLogicApp/RK.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreLogicApp
+{
+    internal class RK : Entity
+    {
+        private int parameterRK;
+        public RK(int parameterRK)
+        {
+            this.parameterRK = parameterRK;
+        }
+
+        public RK()
+        {
+            this.parameterRK = 0;
+        }
+
+        public int ParameterRK
+        {
+            get { return parameterRK; }
+            set { parameterRK = value; }
+        }
+    }
+}
